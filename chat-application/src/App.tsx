9dcdf2c3563b8ad62d40e406cd01071ad0263cb6
Armaan Mathew <armaan.mathew@neoito.com>
2022-04-18 11:41:23 +0530

    first commit

---
+++ chat-application/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatList from './components/ChatList';
+
+function App() {
+  return (
+    <div className=''>
+      <ChatList />
+    </div>
+  );
+}
+
+export default App;
